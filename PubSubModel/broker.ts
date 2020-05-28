7b64f513263dd1f7c021f911e69bfc32a012ddf3
zh <zhihuei94@gmail.com>
2020-05-28 18:04:25 +0800

    seperate subscriber instance and add replay feature

--- PubSubModel/broker.ts
+++ PubSubModel/broker.ts
@@ -1,35 +1,125 @@
-interface ISubscriber {
-    channel: string;
-    id: string;
-    callback: ICallback;
-}
-
 interface ICallback {
     (message: string): void;
 }
 
+enum Status {
+    Active = 'active',
+    Pending = 'pending',
+    Inactive = 'inactive'
+}
+
+interface ISubscriber {
+    getChannel(): string;
+    getId(): string;
+    invokeCallback(data: string): void;
+    getStatus(): string;
+    ack(): void;
+}
+
+export class Subscriber implements ISubscriber {
+    private channel: string;
+    private id: string;
+    private callback: ICallback;
+    private status: Status;
+    private counter = 0;
+
+    constructor(channel: string, id: string, callback: ICallback) {
+        this.channel = channel;
+        this.id = id;
+        this.callback = callback;
+        this.status = Status.Active;
+    }
+
+    public getChannel(): string {
+        return this.channel;
+    }
+    
+    public getId(): string {
+        return this.id;
+    }
+
+    public getStatus(): string {
+        return this.status;
+    }
+
+    public ack(): void {
+        this.status = Status.Active;
+    }
+
+    public invokeCallback(data: string): void {
+        this.status = Status.Pending;
+        this.callback(data);
+        this.startReplayTimer(data);
+    }
+
+    private startReplayTimer(data: string): void {
+        const handle = setTimeout(() => {
+            if (this.replayIfPending(data) || ++this.counter === 3) {            
+                clearTimeout(handle);
+            } else {
+                this.startReplayTimer(data);
+            }
+        }, 5000);
+    }
+
+    private replayIfPending(data: string): boolean {
+        if (this.status === Status.Pending) {
+            console.log('Replaying data to', this.id, 'at', this.channel, ' ........');
+            this.callback(data);
+            return false;
+        }
+        return true;
+    }
+}
+
 class MessageBroker {
     private subscribers: ISubscriber[] = [];
-    public registerSubscriber(channel: string, id: string, callback: ICallback): void {
-        const subscriber: ISubscriber = {
-            channel,
-            id,
-            callback
-        }
+    public registerSubscriber(subscriber: Subscriber): void {
         this.subscribers.push(subscriber);
     }
 
     public publish(channel: string, data: string): void {
-        this.subscribers
-            .filter((subscriber) => channel === subscriber.channel)
-            .forEach((subscriber) => subscriber.callback(data));
+        this.subscribers.filter((subscriber) => subscriber.getStatus() === Status.Active && subscriber.getChannel() === channel)
+            .forEach((subscriber) => subscriber.invokeCallback(data));
     }
 
-    public unsubscribe(id: string) {
-        const indexes = this.subscribers
-            .reduce((acc, subscriber, i) => (subscriber.id === id)? acc.concat(i) : acc, [] as number[]);
-        indexes.forEach((index) => this.subscribers.splice(index, 1));
+    public unsubscribe(id: string, channel?: string) {
+        if (channel) {
+            const index = this.subscribers.findIndex((subscriber) => subscriber.getId() === id && subscriber.getChannel() === channel); 
+            this.subscribers.splice(index, 1);
+        } else {
+            const indexes = this.subscribers
+                .reduce((acc, subscriber, i) => (subscriber.getId() === id)? acc.concat(i) : acc, [] as number[]);
+            indexes.forEach((toRemove) => this.subscribers.splice(toRemove, 1));
+        }
     }
 }
 
 export const broker = new MessageBroker();
+
+// public replayPublish(data: string): void {
+//     let x = 0;
+//     const handle = setInterval(() => {
+//         if (this.retryIfPending(data) || ++x === 10) {
+//             clearInterval(handle);
+//         }
+//     }, 5000);
+// }
+
+// private retryIfPending(data: string): boolean {
+//     for (const subscriber of this.subscribers) {
+//         console.log(subscriber);
+//         if (subscriber.getStatus() === Status.Pending) {
+//             console.log('replaying publish to...', subscriber.getId());
+//             subscriber.invokeCallback(data);
+//             return false;
+//         }
+//     }
+//     console.log('will it reach here?');
+//     return true;   // all subscribers are active
+// }
+
+// public sendACK(id: string, channel: string): void {
+//     const index = this.subscribers.findIndex((subscriber) => subscriber.getId() === id && subscriber.getChannel() === channel);
+//     this.subscribers[index].setStatus(Status.Active);
+// }
--- PubSubModel/main.ts
+++ PubSubModel/main.ts
@@ -1,21 +1,37 @@
-import { broker } from './broker';
+import { broker, Subscriber } from './broker';
 
-// SubscriberA 
-broker.registerSubscriber('aChannel', 'subscriberA', (data) => {
-    console.log('aChannel -> subscribedA', data);
-});
-broker.registerSubscriber('bChannel', 'subscriberA', (data) => {
-    console.log('bChannel -> subscribedA', data);
-});
+// SubscriberA
+const callbackA_K = (data: string): void => {
+    console.log('subcriberA: Message from ChannelK', data);
+    subcriberA_K.ack();
+};
+const callbackA_J = (data: string): void => {
+    console.log('subcriberA: Message from ChannelJ', data);
+    subcriberA_J.ack();
+};
+const subcriberA_K = new Subscriber('ChannelK', 'subA', callbackA_K);
+const subcriberA_J = new Subscriber('ChannelJ', 'subA', callbackA_J);
 
 // SubscriberB
-broker.registerSubscriber('aChannel', 'subscriberB', (data) => {
-    console.log('aChannel -> subscribedB', data);
-});
-broker.registerSubscriber('bChannel', 'subscriberB', (data) => {
-    console.log('bChannel -> subscribedB', data);
-});
-broker.unsubscribe('subscriberA');
+const callbackB_K = (data: string): void => {
+    console.log('subcriberB: Message from ChannelK', data);
+    subcriberB_K.ack();
+};
+const callbackB_J = (data: string): void => {
+    console.log('subcriberB: Message from ChannelJ', data);
+    // subcriberB_J.ack();
+};
+const subcriberB_K = new Subscriber('ChannelK', 'subB', callbackB_K);
+const subcriberB_J = new Subscriber('ChannelJ', 'subB', callbackB_J);
+
+broker.registerSubscriber(subcriberA_K);
+broker.registerSubscriber(subcriberA_J);
+broker.registerSubscriber(subcriberB_K);
+broker.registerSubscriber(subcriberB_J);
+
 // Publisher
-broker.publish('aChannel', "Hello World");
-broker.publish('bChannel', 'Hello There');
+broker.publish('ChannelK', "Message K");
+broker.publish('ChannelJ', "Message J");
+
+// broker.publish('aChannel', "Hello World222");
+// broker.publish('bChannel', 'Hello There');
